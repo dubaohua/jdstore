10ac2cca3b7add0267a8759a9bd4396cc6d50106
baohua <113983633@qq.com>
2017-01-25 09:46:43 +0800

    create controller & model admin::products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125005759) do
+ActiveRecord::Schema.define(version: 20170125014514) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "quantity"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
