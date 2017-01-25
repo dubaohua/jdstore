a95afa350f03e4db1b30db9d521bc939b740f70f
baohua <113983633@qq.com>
2017-01-25 20:55:03 +0800

    implement backend CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125014514) do
+ActiveRecord::Schema.define(version: 20170125080539) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "quantity"
+    t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
